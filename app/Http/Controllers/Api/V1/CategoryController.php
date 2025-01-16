b5e8a8b069afa9844d8ab3719f2c0ebae94bd8d3
Vladislav <vladislavlotockij1@gmail.com>
2025-01-16 15:11:48 +0200

    added update method for category

--- app/Http/Controllers/Api/V1/CategoryController.php
+++ app/Http/Controllers/Api/V1/CategoryController.php
@@ -38,7 +38,8 @@ class CategoryController extends Controller
      */
     public function update(UpdateCategoryRequest $request, Category $category)
     {
-        //
+        $category -> update($request->all());
+        return $category;
     }
 
     /**
